1737ddfd08b1c2b488721ae034f9459eeb499099
Maciej <maciek2tr99@gmail.com>
2019-12-10 21:24:30 +0100

    [Add] get random MoveDirection

--- src/main/agh/cs/oop1/MoveDirection.java
+++ src/main/agh/cs/oop1/MoveDirection.java
@@ -1,8 +1,16 @@
 package agh.cs.oop1;
 
+import java.util.Random;
+
 public enum MoveDirection {
     FORWARD,
     BACKWARD,
     RIGHT,
-    LEFT
+    LEFT;
+
+    public static MoveDirection getRandomDirection(){
+        Random rand = new Random();
+        return MoveDirection.values()[rand.nextInt(MoveDirection.values().length)];
+    }
+
 }
