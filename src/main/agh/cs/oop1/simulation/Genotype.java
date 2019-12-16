a0515c2d0f1110e75d4f4133f8d331de3b06821a
Maciej <maciek2tr99@gmail.com>
2019-12-16 22:21:49 +0100

    [Add] Simple methods in Genotype

--- src/main/agh/cs/oop1/simulation/Genotype.java
+++ src/main/agh/cs/oop1/simulation/Genotype.java
@@ -1,20 +1,38 @@
 package agh.cs.oop1.simulation;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class Genotype {
-    private static byte maxGenValue=7;
+    private static final byte maxGenValue = 7;
+    private static final int genotypeSize = 32;
 
-    private byte[] genotype = new byte[32];
-
+    private static final Random rand = new Random();
+    private byte[] genotype = new byte[genotypeSize];
 
     public static Genotype getRandomGenotype(){
-        Random rand = new Random();
         Genotype gen = new Genotype();
 
-        for(int i = 0; i< gen.genotype.length; i++){
-            gen.genotype[i] = (byte)rand.nextInt(maxGenValue);
-        }
+        for(int i = 0; i< gen.genotype.length; i++)
+            gen.genotype[i] = (byte)Genotype.rand.nextInt(maxGenValue);
+
         return gen;
     }
+
+    public String toString(){
+        StringBuilder res= new StringBuilder();
+        for(byte b : this.genotype)
+            res.append((char) (b + (int) '0'));
+        return res.toString();
+    }
+
+    public boolean equals(Object other){
+        if (this == other)
+            return true;
+        if (!(other instanceof Genotype))
+            return false;
+        Genotype that = (Genotype) other;
+        return Arrays.equals(this.genotype, that.genotype);
+    }
+
 }
